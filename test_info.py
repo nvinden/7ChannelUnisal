70fe8946424970c77be504fb7cf27613db23cde7
nvinden <nvinden@uoguelph.ca>
2021-05-13 21:58:05 -0400

    nvinden

--- test_info.py
+++ test_info.py
@@ -12,7 +12,9 @@ DATASET_PATH = "/content/drive/MyDrive/7Channel/Salicon"
 # chan: the number of channels the image can produce, either 1 or 3
 CHANNELS = (
     {"dir": "depth_kitti", "func": "depth_kitti", "end": "png", "chan": 1},
-    {"dir": "depth_nyu", "func": "depth_nyu", "end": "png", "chan": 1}
+    {"dir": "depth_nyu", "func": "depth_nyu", "end": "png", "chan": 1},
+    {"dir": "dark", "func": "make_dark_layer", "end": "jpg", "chan": 3},
+    {"dir": "rgb", "func": "make_rgb_mean_layer", "end": "jpg", "chan": 3}
 )
 
 #Note: All datasets must be in the format DATASET_PATH/dir_name/(train test and val)
