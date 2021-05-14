8fa2e0df0640707501a0e52f70b284772265ce3f
nvinden <nvinden@uoguelph.ca>
2021-05-14 13:58:50 -0400

    nvinden

--- unisal/sevenchanneltrans.py
+++ unisal/sevenchanneltrans.py
@@ -93,12 +93,12 @@ class SevenChannelTrans(object):
         return dark.unsqueeze(0)
     
     def depth_kitti(self, img):
-        im = Image.fromarray(img)
+        im = Image.fromarray(img.numpy())
         _, predicted_depth = self.kitti_helper.predict_pil(im)
         return predicted_depth
 
     def depth_nyu(self, img):
-        im = Image.fromarray(img)
+        im = Image.fromarray(img.numpy())
         im.save("TEST_NYU.png")
         _, predicted_depth = self.nyu_helper.predict_pil(im)
         return predicted_depth
