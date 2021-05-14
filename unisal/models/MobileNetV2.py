b55bd5b62304e0b488a43c41ca390b03de33e2cc
nvinden <nvinden@uoguelph.ca>
2021-05-13 21:48:46 -0400

    nvinden

--- unisal/models/MobileNetV2.py
+++ unisal/models/MobileNetV2.py
@@ -164,11 +164,9 @@ class MobileNetV2(nn.Module):
                 Path(__file__).resolve().parent / 'weights/mobilenet_v2.pth.tar')
             first_weight = state_dict['features.0.0.weight']
             state_dict['features.0.0.weight'] = torch.cat((first_weight[:,0:3,:,:], torch.zeros((self.input_channel, 3 + num_input_channels, 3, 3), device="cuda:0")), 1)
-            '''
             print(state_dict['features.0.0.weight'].size())
             for param_tensor in state_dict:
                 print(param_tensor, "\t", state_dict[param_tensor].size())
-            '''
             self.load_state_dict(state_dict, strict=False)
         else:
             self._initialize_weights()
