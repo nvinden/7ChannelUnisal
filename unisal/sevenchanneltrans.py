5e989ef8e2aa6eb4afa0f32a7d7844add250e60d
nvinden <nvinden@uoguelph.ca>
2021-05-18 11:01:39 -0400

    nvinden

--- unisal/sevenchanneltrans.py
+++ unisal/sevenchanneltrans.py
@@ -68,8 +68,7 @@ class SevenChannelTrans(object):
                     new_channel = transforms.Resize((height, width))(new_channel)
                 save_image(new_channel, channel_path)
                 image = torch.cat((image, new_channel), 0)
-        
-        print(".", end="")
+            print(".", end="")
 
         return image
 
